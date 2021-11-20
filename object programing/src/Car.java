2da7d051252ca669d4264dfb3c919f54f0ef206b
Melisa <melisa94@interia.pl>
2021-11-20 11:46:17 +0100

    pierwszy commit

---
+++ object programing/src/Car.java
@@ -0,0 +1,8 @@
+public class Car {                                           //nowa klasa car, teraz chcemy uruchomić to w klasie main//
+    public void startEngine(){                   // publiczna metoda bez wartości do zwrócenia, chcemy tylko uruchomić//
+        System.out.println("Silnik działa");
+    }
+    public void stopEngine(){
+        System.out.println("Silnik zgasł");
+    }
+}
---
+++ object programing/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+      Car audi = new Car();   //tworzymy obiekt typu car//
+        audi.startEngine();
+        audi.stopEngine();
+    }
+}
